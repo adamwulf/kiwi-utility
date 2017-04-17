49f368587bf65ce5eaa19fce3a370c0d76d27479
Adam Wulf <adam.wulf@gmail.com>
2017-04-17 14:55:55 -0500

    Moved to static framework

---
+++ KiwiUtility/KiwiUtility.h
@@ -0,0 +1,21 @@
+//
+//  KiwiUtility.h
+//  KiwiUtility
+//
+//  Created by Adam Wulf on 4/17/17.
+//  Copyright © 2017 Milestone Made. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KiwiUtility.
+FOUNDATION_EXPORT double KiwiUtilityVersionNumber;
+
+//! Project version string for KiwiUtility.
+FOUNDATION_EXPORT const unsigned char KiwiUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KiwiUtility/PublicHeader.h>
+
+
+#import <KiwiUtility/NSArray+MapReduce.h>
+#import <KiwiUtility/NSArray+Helper.h>
