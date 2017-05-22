d921562cfd1614bf8462d429e636ce8bdd23b0f8
Adam Wulf <adam.wulf@gmail.com>
2017-05-22 17:05:04 -0500

    Added method to hash doubles

--- KiwiUtility/KiwiUtility.h
+++ KiwiUtility/KiwiUtility.h
@@ -16,10 +16,18 @@ FOUNDATION_EXPORT const unsigned char KiwiUtilityVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <KiwiUtility/PublicHeader.h>
 
-
 #import <KiwiUtility/NSArray+MapReduce.h>
 #import <KiwiUtility/NSArray+Helper.h>
 #import <KiwiUtility/NSString+MMHelper.h>
 #import <KiwiUtility/NSFileManager+DirectoryOptimizations.h>
 #import <KiwiUtility/MMStatTracker.h>
 #import <KiwiUtility/Pair.h>
+
+static inline NSUInteger HashDouble(double value) {
+    double absolute = ABS(value);
+    double integral = round(absolute);
+    double fragment = absolute - integral;
+    NSUInteger integralHash = 2654435761U * fmod(integral, NSUIntegerMax);
+    NSUInteger fragmentHash = fragment * NSUIntegerMax;
+    return integralHash + fragmentHash;
+}
